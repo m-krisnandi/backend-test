4d3bd9492a5ae03986fd288b43b6dc51d8dde5af
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-07-02 17:34:59 +0700

    refactor: env database

--- app/configs/index.js
+++ app/configs/index.js
@@ -4,4 +4,11 @@ dotenv.config();
 module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiration: process.env.JWT_EXPIRATION,
+  dbConfig: {
+    dbName: process.env.DB_NAME,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbDialect: process.env.DB_DIALECT,
+  },
 };
--- app/db/index.js
+++ app/db/index.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require("sequelize");
+const { dbConfig } = require("../configs");
 
-const db = new Sequelize("vehicle_db", "postgres", "secret", {
-  host: "localhost",
-  dialect: "postgres",
+const { dbName, dbUser, dbPassword, dbHost, dbDialect } = dbConfig;
+
+const db = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: dbDialect,
 });
 
 // auto generate table to database
